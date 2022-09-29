755a8cd87efb33b29b3c16808b6183091c6b186e
Jocelyn Ng <jng@hubspot.com>
2022-09-29 16:06:02 +0800

    form api added

--- app.js
+++ app.js
@@ -210,6 +210,56 @@ app.post('/admin', async (req, res) => {
 
 app.post('/', (req, res) => {
 	console.log('started');
+
+	function formv3(){
+		// Create the new request 
+		var xhr = new XMLHttpRequest();
+		var url = 'https://api.hsforms.com/submissions/v3/integration/submit/4718896/4f836c1a-cc7f-4df6-92fd-52d2f34f3e8d'
+		    // Example request JSON:
+			var data = {
+				"fields": [
+				  {
+					"objectTypeId": "0-1",
+					"name": "email",
+					"value": req.body.email
+				  },
+				  {
+					"objectTypeId": "0-1",
+					"name": "firstname",
+					"value": req.body.firstname
+				  }
+				],
+				"context": {
+				  "hutk": req.cookies.hubspotutk,
+				  "pageUri": "https://dark-bee-dirndl.cyclic.app/contact",
+				  "pageName": "Portfolio app contact page"
+				}
+			  }
+		  
+			  var final_data = JSON.stringify(data)
+		  
+			  xhr.open('POST', url);
+			  // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+			  xhr.setRequestHeader('Content-Type', 'application/json');
+		  
+			  xhr.onreadystatechange = function() {
+				  if(xhr.readyState == 4 && xhr.status == 200) { 
+					  console.log(xhr.responseText); // Returns a 200 response if the submission is successful.
+				  } else if (xhr.readyState == 4 && xhr.status == 400){ 
+					  console.log(xhr.responseText); // Returns a 400 error the submission is rejected.          
+				  } else if (xhr.readyState == 4 && xhr.status == 403){ 
+					  console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
+				  } else if (xhr.readyState == 4 && xhr.status == 404){ 
+					  console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+				  }
+				 }
+		  
+		  
+			  // Sends the request 
+			  
+			  xhr.send(final_data)
+	}
+	formv3(); 
 	collection.insertOne(req.body, (err, result) => {  
         if (err) return console.log(err)
     
