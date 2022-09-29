c01478d613437284125aeba76af6a72fc034d0cf
Jocelyn Ng <jng@hubspot.com>
2022-09-29 15:55:04 +0800

    removed form

--- app.js
+++ app.js
@@ -213,77 +213,12 @@ app.post('/', (req, res) => {
 	collection.insertOne(req.body, (err, result) => {  
         if (err) return console.log(err)
     
-        console.log('saved to database');        
-      })
+        console.log('saved to database'); 
+		res.redirect('/'); // or do something else here		       
+       })
 
-	  function formv3(){
-		// Create the new request 
-		var xhr = new XMLHttpRequest();
-		var url = 'https://api.hsforms.com/submissions/v3/integration/submit/4718896/4f836c1a-cc7f-4df6-92fd-52d2f34f3e8d'
-		
-		// Example request JSON:
-		var data = {
-		  "submittedAt": "1517927174000",
-		  "fields": [
-			{
-			  "objectTypeId": "0-1",
-			  "name": "email",
-			  "value": "example@example.com"
-			},
-			{
-			  "objectTypeId": "0-1",
-			  "name": "firstname",
-			  "value": "Jeff"
-			}
-		  ],
-		  "context": {
-			"hutk": ':hutk', // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
-			"pageUri": "www.example.com/page",
-			"pageName": "Example page"
-		  },
-		  "legalConsentOptions":{ // Include this object when GDPR options are enabled
-			"consent":{
-			  "consentToProcess":true,
-			  "text":"I agree to allow Example Company to store and process my personal data.",
-			  "communications":[
-				{
-				  "value":true,
-				  "subscriptionTypeId":999,
-				  "text":"I agree to receive marketing communications from Example Company."
-				}
-			  ]
-			}
-		  }
-		}
-	
-		var final_data = JSON.stringify(data)
-	
-		xhr.open('POST', url);
-		// Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
-		xhr.setRequestHeader('Content-Type', 'application/json');
-	
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4 && xhr.status == 200) { 
-				alert(xhr.responseText); // Returns a 200 response if the submission is successful.
-			} else if (xhr.readyState == 4 && xhr.status == 400){ 
-				alert(xhr.responseText); // Returns a 400 error the submission is rejected.          
-			} else if (xhr.readyState == 4 && xhr.status == 403){ 
-				alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
-			} else if (xhr.readyState == 4 && xhr.status == 404){ 
-				alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
-			}
-		   }
 	
 	
-		// Sends the request 
-		
-		xhr.send(final_data)
-	 }
-	 
-	console.log(req.body.email + ' ' + req.cookies.hubspotutk);
-	formv3();
-	res.redirect('/'); // or do something else here		       
- 
 });
 
 
